e474455d49946b599d9fa82b1bd972ec41063a3d
Arthur Cerqueira <arthurcerquer@gmail.com>
2020-04-20 18:58:37 -0300

    Condições (Parte 1)

---
+++ móduloD/aula11/ex008.js
@@ -0,0 +1,10 @@
+/*Escrever na tela com o NODEJS:
+console.log('O console funcionou corretamente')
+*/
+
+var vel = 60.1
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){ //Condição Simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
