d43aef918293e50b7140712b9c755e60afb7c40b
Arthur Cerqueira <arthurcerquer@gmail.com>
2020-05-09 20:36:26 -0300

    Repetições (Parte 1)

---
+++ móduloE/aula13/ambiente.js
@@ -0,0 +1,20 @@
+var contador = 1
+do {
+    console.log(`Contando... ${contador}`)
+    contador++
+} while (contador <= 6)
+console.log(`Contei até ${contador - 1} vezes`)
+
+/*var contador = 1
+ while (contador <= 6){
+     console.log(`Contando... ${contador}`)
+     contador++ // contador = contador + 1   
+ }
+console.log(`Contei até ${contador - 1} vezes`)
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
